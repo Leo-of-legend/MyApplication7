4644e0b8de967b068376511e97103be421397c83
leo-of-legend <esaympwndud@gmail.com>
2018-12-06 19:56:15 +0900

    add sub Activity

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ACTIVITY_LC","onCreate호출됨");
+        Toast.makeText(getApplicationContext(),"onCreate호출됨", Toast.LENGTH_SHORT).show();
+
+        Button button = (Button) findViewById(R.id.newActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LeoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
